5dfac564dd90f5e0b479c7e862f251bd0f0611c9
EvanShui <evanshui0@gmail.com>
2017-12-30 17:38:02 -0800

    account for renamed module

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
     imports the app variable from the Flask instance called stock_analysis_app from
     stock_analysis_app/__init__.py, and call app.run()
 '''
-from stock_analysis_app import app
+from app import app
 
 if __name__ == '__main__':
     app.run()
